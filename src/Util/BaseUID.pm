841d77367b0ea553518f80deed55003795064769
Brenton Scott <admin@trixarian.net>
2013-09-12 18:11:55 +0200

    Update BaseUID.pm

--- src/Util/BaseUID.pm
+++ src/Util/BaseUID.pm
@@ -139,8 +139,6 @@ sub _request_nick {
 		my $tagtest = Setting::get(tagsep => $net);
 		$tagsep = $tagtest if $tagtest eq '_';
 		$reqnick =~ s/\//\|/g if $reqnick =~ /\//g && $tagtest eq '_';
-		# The shit I do to deal with multiple Januses:
-		$reqnick =~ s/(\/[a-zA-Z0-9]+){1,}/-ln/g if $reqnick =~ /(\/[a-zA-Z0-9]+){1,}/g;
 		my $tag = $tagsep . $nick->homenet()->name();
 		my $i = 0;
 		$given = substr($reqnick, 0, $maxlen - length $tag) . $tag;
