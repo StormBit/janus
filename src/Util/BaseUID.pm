01a2fbc2d858ea593215ce76a7e7d001d4c50db4
Brenton Scott <admin@trixarian.net>
2013-09-12 15:06:04 +0200

    Update BaseUID.pm

--- src/Util/BaseUID.pm
+++ src/Util/BaseUID.pm
@@ -116,8 +116,8 @@ sub register_nick {
 sub _request_nick {
 	my($net, $nick, $reqnick, $tagged) = @_;
 	# The shit I do to deal with multiple Januses:
-	$reqnick =~ s/(\/[a-zA-Z0-9]+){2,}/-ln/ if $reqnick =~ /(\/[a-zA-Z0-9]+){2,}/;
-	$reqnick =~ s/\//\|/ if $reqnick =~ /\// && $Janus::tagfix;
+	$reqnick =~ s/(\/[a-zA-Z0-9]+){2,}/-ln/g if $reqnick =~ /(\/[a-zA-Z0-9]+){2,}/g;
+	$reqnick =~ s/\//\|/g if $reqnick =~ /\//g && $Janus::tagfix;
 
 	my $maxlen = $net->nicklen();
 	my $given = substr $reqnick, 0, $maxlen;
@@ -141,7 +141,7 @@ sub _request_nick {
 		my $tagsep = $Janus::septag;
 		my $tagtest = Setting::get(tagsep => $net);
 		$tagsep = $tagtest if $tagtest eq '_';
-		$reqnick =~ s/\//\|/ if $reqnick =~ /\// && $tagtest eq '_';
+		$reqnick =~ s/\//\|/g if $reqnick =~ /\//g && $tagtest eq '_';
 		my $tag = $tagsep . $nick->homenet()->name();
 		my $i = 0;
 		$given = substr($reqnick, 0, $maxlen - length $tag) . $tag;
