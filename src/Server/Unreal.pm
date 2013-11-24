9c30b2e442ff54ee29080f8d0a8edb3690398737
Brenton Scott <admin@trixarian.net>
2013-11-24 13:28:04 +0200

    Ignoring numerics we don't use in Unreal

--- src/Server/Unreal.pm
+++ src/Server/Unreal.pm
@@ -378,7 +378,8 @@ sub nick_msg {
 		$msg->[0] = $about;
 	}
 	if ($_[2] =~ /\./) {
-		Log::warn_in($net, 'numeric', @_);
+		# Why do we care about numerics we don't use?
+		# Log::warn_in($net, 'numeric', @_);
 		return ();
 	}
 	my $dst = $net->nick($_[2]) or return ();
